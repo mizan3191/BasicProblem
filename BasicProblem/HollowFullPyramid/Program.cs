29fec3b7b8c7b384440e9df62c738bca5a33d0fd
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2022-04-14 10:46:40 +0600

    save

--- BasicProblem/HollowFullPyramid/Program.cs
+++ BasicProblem/HollowFullPyramid/Program.cs
@@ -6,10 +6,45 @@ namespace HollowFullPyramid
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your Pyramid Hight :");
+            Console.WriteLine("Enter your Hollow Full Pyramid Hight :");
             var Hight = int.Parse(Console.ReadLine());
             var k = 0;
 
+            for (int i = Hight; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if(i == Hight || i<=2)
+                    {
+                        Console.Write("* ");
+                    }
+
+                    else if(j == 1)
+                    {
+                        Console.Write("* ");
+                    }
+
+
+                    else if (j == i)
+                    {
+                        Console.Write("* ");
+                        
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+
+                Console.WriteLine();
+                k = Hight;
+
+                while (k != i - 1)
+                {
+                    Console.Write(" ");
+                    k--;
+                }
+            }
 
         }
     }
