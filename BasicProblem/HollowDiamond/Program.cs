89073b98a2e587ffe1b30071bb3dac42ac68b940
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2022-04-16 13:35:21 +0600

    save

---
+++ BasicProblem/HollowDiamond/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HollowDiamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Solid Diamond Even Hight :");
+            var Hight = int.Parse(Console.ReadLine());
+            Hight = Hight / 2;
+
+            for(int i = 0; i < Hight; i++)
+            {
+                for(int j = 0; j < Hight; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
--- BasicProblem/SolidDiamond/Program.cs
+++ BasicProblem/SolidDiamond/Program.cs
@@ -6,7 +6,41 @@ namespace SolidDiamond
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter Your Solid Diamond Even Hight :");
+            var Hight = int.Parse(Console.ReadLine());
+            Hight = Hight/2;
+            var space = 0;
+
+            for (int i = 1; i <= Hight; i++)
+            {
+                for(space = 0; space <= Hight-i; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j != i; j++)
+                {
+                    Console.Write("* ");
+
+                }
+
+                Console.WriteLine();
+            }
+
+            for(int i = Hight; i > 0; i--)
+            {
+                for (space = i; space <= Hight; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                
+                Console.WriteLine();
+            }
         }
     }
 }
