08284028c2da8c1602f4b0672583fb37fa652110
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2022-04-17 10:15:36 +0600

    save

--- BasicProblem/HollowDiamond/Program.cs
+++ BasicProblem/HollowDiamond/Program.cs
@@ -10,12 +10,50 @@ namespace HollowDiamond
             var Hight = int.Parse(Console.ReadLine());
             Hight = Hight / 2;
 
-            for(int i = 0; i < Hight; i++)
-            {
-                for(int j = 0; j < Hight; j++)
-                {
+            var space = 0;
 
+
+            for (int i = 1; i <= Hight; i++)
+            {
+                for(space = i; space < Hight; space++)
+                {
+                    Console.Write(" ");
                 }
+
+                for (int j = 1; j != i+1; j++)
+                {
+                    if(j == 1 || j == i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = Hight; i > 0; i--)
+            {
+                for (space = i; space < Hight; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = i; j>0; j--)
+                {
+                    if (j == 1 || j == i || i <= 2)
+                    {
+                        Console.Write("* ");
+                    }
+
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
             }
         }
     }
